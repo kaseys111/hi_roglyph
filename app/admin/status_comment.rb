aeed908dde99702696a779c01989b0d28b01f3db
Raghu Betina <raghu@firstdraft.com>
2016-11-02 02:35:38 +0000

    Whitelist columns in StatusComment for ActiveAdmin

--- app/admin/status_comment.rb
+++ app/admin/status_comment.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register StatusComment do
 
+ permit_params :status_id, :comment, :commenter_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
